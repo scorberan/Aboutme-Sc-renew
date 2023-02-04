8917e8f6314c3617793e7797d8637e0f83327b9c
scorberan <sarahcorberan@gmail.com>
2023-02-04 18:10:42 +0100

    Copy of the project Aplha

---
+++ projectalpha-sarahcorberan.js
@@ -0,0 +1,70 @@
+
+  // Multi-action btn 
+  //enable all menu btn
+
+  document.querySelectorAll(".mab").forEach(multiAction => {
+    const menuButton = multiAction.querySelector(".mab-btn-menu");
+    const list = multiAction.querySelector(".mab-list");
+
+   menuButton.addEventListener("click", () => {
+    list.classList.toggle("mab-list--visible");
+
+   });
+  });
+
+  //Hide all lists when clicking elsewhere on the page
+
+  document.addEventListener("click",e => {
+    const keepOpen = (
+        e.target.matches(".mab-list")
+      || e.target.matches(".mab-btn-menu")
+      || e.target.closest(".mab-btn-menu")
+    );
+    
+    if (keepOpen) return;
+    document.querySelectorAll(".mab-list").forEach(list => {
+      list.classList.remove("mab-list--visible");
+    });
+  });
+
+  document.querySelectorAll('[href^="#"]').forEach(menuButton => {
+    menuButton.addEventListener('click', function (event) {
+      event.preventDefault();
+      // esto es un id
+      const sectionId = this.getAttribute('href');
+      const sectionTopPosition = document.querySelector(sectionId).offsetTop;
+      window.scroll({
+        top: sectionTopPosition,
+        behavior: 'smooth'
+      });
+      
+    });
+  });
+
+ 
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
